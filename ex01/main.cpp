ca837a043fe657b8e332430e2f26fd3d3b6f68c8
¨mhummel7¨ <mhummel@student.42heilbronn.de>
2025-10-22 13:51:57 +0200

    Refactor main function to improve argument handling and error messaging

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,9 +6,25 @@
 /*   By: mhummel <mhummel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 13:05:49 by mhummel           #+#    #+#             */
-/*   Updated: 2025/10/22 13:07:04 by mhummel          ###   ########.fr       */
+/*   Updated: 2025/10/22 13:09:38 by mhummel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
 #include <iostream>
+
+int main(int argc, char** argv) {
+	if (argc != 2) {
+		std::cerr << "Error: invalid arguments. Usage: ./rpn <expression>" << std::endl;
+		return 1;
+	}
+	RPN rpn;
+	try {
+		int result = rpn.evaluate(argv[1]);
+		std::cout << result << std::endl;
+	} catch (const std::runtime_error& e) {
+		std::cerr << e.what() << std::endl;
+		return 1;
+	}
+	return 0;
+}
