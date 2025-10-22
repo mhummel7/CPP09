5f84943e90c527e8bb714bad2accb5c93a1d5e77
¨mhummel7¨ <mhummel@student.42heilbronn.de>
2025-10-22 09:40:05 +0200

    Adds class definition and header guards for exchange logic
    Introduces a class interface to manage exchange rate data, input validation, and file operations.

--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -6,7 +6,31 @@
 /*   By: mhummel <mhummel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 09:25:41 by mhummel           #+#    #+#             */
-/*   Updated: 2025/10/22 09:25:42 by mhummel          ###   ########.fr       */
+/*   Updated: 2025/10/22 09:40:01 by mhummel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include <map>
+#include <string>
+
+class BitcoinExchange {
+	private:
+		std::map<std::string, float> _db;
+
+		bool isValidDate(const std::string& date);
+		float getExchangeRate(const std::string& date);
+		std::string trim(const std::string& str);
+
+	public:
+		BitcoinExchange();
+		BitcoinExchange(const BitcoinExchange& other);
+		BitcoinExchange& operator=(const BitcoinExchange& other);
+		~BitcoinExchange();
+
+		void loadDatabase(const std::string& inputFile);
+};
+
+#endif
